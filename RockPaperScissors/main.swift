8d45477387dedcb726cd49cc54687b925eefb772
ChanHyuc <whk1140@gmail.com>
2024-07-22 21:07:51 +0900

    Add: 결과 확인을 위한 print 문 추가

--- RockPaperScissors/main.swift
+++ RockPaperScissors/main.swift
@@ -15,18 +15,24 @@ private func playGame() {
             continue
         }
         
-        let userNumber = RockPaperScissors(rawValue: userInput)
+        guard let userNumber = RockPaperScissors(rawValue: userInput) else { return }
         
         if userInput == 0 {
             break;
         } else if randomNumber == userNumber {
+            print("컴퓨터: \(randomNumber)")
+            print("사용자: \(userNumber)")
             print("비겼습니다!")
         } else if (randomNumber == .rock && userNumber == .paper) ||
                     (randomNumber == .paper && userNumber == .scissors) ||
                     (randomNumber == .scissors && userNumber == .rock) {
+            print("컴퓨터: \(randomNumber)")
+            print("사용자: \(userNumber)")
             print("이겼습니다!")
         }
         else {
+            print("컴퓨터: \(randomNumber)")
+            print("사용자: \(userNumber)")
             print("졌습니다!")
         }
         
